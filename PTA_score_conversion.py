b1f69ff9fa5b2ce28fa015f12bb953ad59c7518b
DingZhan <37229890+DingZhan@users.noreply.github.com>
2019-12-25 10:40:13 +0800

    PTA乙级成绩转换成课程期末成绩

---
+++ PTA_score_conversion.py
@@ -0,0 +1,37 @@
+#import os
+file = open("score.txt", "w")
+b = e = 1.0;
+while(True):
+    score = int(input())
+#for score in range(0, 100):
+#    print("{0} ".format(score))
+    if score<0:
+        break
+    if score<35:
+        e = 50/34.0;
+        b = e
+        ratio = (score-0.0)/34
+    elif score<50:
+        b = 60/35.0
+        e = 79/49.0
+        ratio = (score-35.0)/(49-35)
+    elif score<75:
+        b = 80/50.0
+        e = 89/74.0
+        ratio = (score-50.0)/(74-50)
+    elif score<90:
+        b = 90/75.0
+        e = 99/89.0
+        ratio = (score-75.0)/(89-75)
+    else:
+        b = e = 100;
+        ratio = 1.0
+    ratio = b*(1-ratio)+e*ratio;
+    score =int(score*ratio);
+    if score<=5:
+        score = 5
+    elif score>=100:
+        score = 100
+    file.write("{0}\n".format(score));
+    print("{0}\n".format(score))
+file.close();
